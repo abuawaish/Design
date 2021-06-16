aca47cb97d6f485ebd8297839c3bcac4ede7c708
Abuawaish <74604342+abuawaish@users.noreply.github.com>
2021-06-16 10:11:31 +0530

    Add files via upload

---
+++ design4.py
@@ -0,0 +1,12 @@
+import turtle
+ls=["red","yellow","green","cyan","blue","white"]
+t=turtle.Turtle()
+sc=turtle.Screen()
+sc.bgcolor('black')
+t.speed(30)
+for i in range(150):
+	t.color(ls[i%6])
+	t.forward(i*4)
+	t.left(150)
+	t.width(2)
+	
